eb90ac127f5bb8de3f58893107d7e8b10a3b3ad7
w1oxn-umcs <w1oxn.stuff@gmail.com>
2026-06-28 22:25:38 +0100

    Update channel ID and message formatting on member add

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,10 +2,15 @@ export default {
     name: "guildMemberAdd",
 
     async execute(member) {
-        const channel = member.guild.channels.cache.get("YOUR_CHANNEL_ID");
+        const channel = member.guild.channels.cache.get("1371492185631100959");
         if (!channel) return;
 
-        const msg = await channel.send(`<@${member.id}>`);
+        const msg = await channel.send({
+            content: `<@${member.id}>`,
+            allowedMentions: {
+                users: [member.id]
+            }
+        });
 
         setTimeout(() => {
             msg.delete().catch(() => {});
