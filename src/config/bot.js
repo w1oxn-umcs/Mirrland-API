ec829b89c77bd852596ac2e1db277c1454e04536
w1oxn-umcs <w1oxn.stuff@gmail.com>
2026-06-28 01:13:31 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Slavēt **Petrov**!",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 4,
       },
     ],
   },
